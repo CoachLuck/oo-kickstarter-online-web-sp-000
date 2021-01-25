e0e7683d6009faa44cfcd9c11243758b999c83e4
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-25 20:55:46 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,16 @@
 class Backer
   attr_accessor :name
   attr_reader :backed_projects
+  @@all = []
 
   def initialize(name)
     @name = name
     @backed_projects = []
+    @@all << self
   end
-  
+
+  def back_project(project)
+    @backed_projects << project
+  end
+
+end
